01e7d5969b890740fc12d0c0c969b0bea5b81f93
Belinda B. Garana <75867404+BelindaBGarana@users.noreply.github.com>
2024-01-12 13:08:26 -0800

    Change default for gene.names parameter

--- R/DMEA_gene_corr.R
+++ R/DMEA_gene_corr.R
@@ -1,6 +1,6 @@
 DMEA_gene_corr <- function(drug.sensitivity, gmt = NULL, expression, weights,
                            value = "AUC", sample.names = colnames(expression)[1], 
-                           gene.names = colnames(data)[1], 
+                           gene.names = colnames(weights)[1], 
                            weight.values = colnames(weights)[2],
                            rank.metric = "Pearson.est", FDR = 0.25,
                            num.permutations = 1000, stat.type = "Weighted",
